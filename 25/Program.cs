92df0e14378aaf501e08cb70c7dc8bd18b2f6ccf
Maxim <max_isupov@mail.ru>
2022-09-30 16:56:08 +0300

    Решил задачу 25

---
+++ 25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int B = int.Parse(Console.ReadLine());
+
+int num = A;
+
+for (int i = 1; i < B; i++)
+{
+num = num * A;
+}
+Console.WriteLine($"После возведения числа {A} в степень {B} - получилось {num}.");
